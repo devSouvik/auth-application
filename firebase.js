810e2430355f0402d26b74de31a74a34dc04716d
Souvik Guria <54489090+devSouvik@users.noreply.github.com>
2021-10-03 21:19:02 +0530

    files uploaded

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAztYC9eMK8a-3prz2KQrrCtlldupSdVN0",
+  authDomain: "webproject-28cdc.firebaseapp.com",
+  projectId: "webproject-28cdc",
+  storageBucket: "webproject-28cdc.appspot.com",
+  messagingSenderId: "460355185594",
+  appId: "1:460355185594:web:125490e5e390a37ecdcb83",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
